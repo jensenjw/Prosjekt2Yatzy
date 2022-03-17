6933f6debf1647bfb0017dec7e772f654e488393
Jan William Jensen <69847071+572018@users.noreply.github.com>
2022-03-17 15:22:43 +0100

    Create RuleTwoPair.java

--- src/main/java/no/hvl/dat109/proj2/yatzy/services/rules/RuleTwoPair.java
+++ src/main/java/no/hvl/dat109/proj2/yatzy/services/rules/RuleTwoPair.java
@@ -2,6 +2,11 @@ package no.hvl.dat109.proj2.yatzy.services.rules;
 
 import java.util.List;
 
+/**
+ * 
+ * @author janwi
+ *	Uses ruleOnePair twice by removing the dices from the first pair
+ */
 public class RuleTwoPair extends RuleOnePair {
 
 	public int resolveTwoPair(List<Integer> list) {
