e5ce50c57082ade220196239a8b05a53ad998a79
Henrik Engdal <591334@stud.hvl.no>
2022-03-18 11:44:06 +0100

    Update diceTest.java

--- src/test/java/diceTest/diceTest.java
+++ src/test/java/diceTest/diceTest.java
@@ -2,6 +2,10 @@ package diceTest;
 
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertFalse;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import no.hvl.dat109.proj2.yatzy.services.rules.RulesADT;
@@ -10,9 +14,8 @@ import rulesTest.RuleThreeOfAKind;
 public class diceTest {
 
 
-	
 	@Test
-	public void testDiceIsNotHigherThan6() {
+	public void testDiceValuesInRange() {
 		
 		//Arrange
 		Dice dice = new Dice();
@@ -21,22 +24,10 @@ public class diceTest {
 		int roll = dice.rollDice();
 		
 		//Assert
-		assertTrue(roll <= 6);
+		assertTrue(roll <= 6 && roll >= 1);
+		
 	}
 	
-	@Test 
-	public void testDiceIsNotLowerThan1() {
-		
-		//Arrange
-		Dice dice = new Dice();
-		
-		//Act
-		int roll = dice.rollDice();
-		
-		//Assert
-		assertTrue(roll >= 1);
-		
-		
-	}
-
 }
+
+
