44ebaf58ea9dc233a1a5dc3aad9dfe0dc46c3387
Jan William Jensen <69847071+572018@users.noreply.github.com>
2022-03-21 12:47:06 +0100

    Update diceTest.java

--- src/test/java/diceTest/diceTest.java
+++ src/test/java/diceTest/diceTest.java
@@ -1,16 +1,10 @@
 package diceTest;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertFalse;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Test;
 
 import no.hvl.dat109.proj2.yatzy.entities.Dice;
-import no.hvl.dat109.proj2.yatzy.services.rules.RulesADT;
-import rulesTest.RuleThreeOfAKind;
 
 public class diceTest {
 
