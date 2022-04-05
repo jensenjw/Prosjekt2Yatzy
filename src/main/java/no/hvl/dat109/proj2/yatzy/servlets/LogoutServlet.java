d8d1b5c1831d1798e9949538f3858a28d7b02b52
Haava <Haava@BYDALL-PC.home>
2022-04-05 14:49:04 +0200

    Må endre destinasjon for logoutServlet. Egen HTML-fil til logout?

--- src/main/java/no/hvl/dat109/proj2/yatzy/servlets/LogoutServlet.java
+++ src/main/java/no/hvl/dat109/proj2/yatzy/servlets/LogoutServlet.java
@@ -2,6 +2,7 @@ package no.hvl.dat109.proj2.yatzy.servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class LogoutServlet
  */
+@WebServlet(name = "logout", urlPatterns = "/logout")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
